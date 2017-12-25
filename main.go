a077fbfcd68ef0fb7da1403a07a40f4db167fee5
Max <sherlockmax226@gmail.com>
2017-12-26 01:23:13 +0800

    update

--- main.go
+++ main.go
@@ -5,23 +5,24 @@ import (
 	"fmt"
 	"log"
 	"sort"
-	"time"
 	"strconv"
+	"time"
 
 	"github.com/coreos/etcd/clientv3"
 	"github.com/spf13/viper"
 )
 
 type monitorCore struct {
-	id         int64
-	name       string
-	host       string
-	port       int
-	heartbeat  int64
-	keyprefix  string
-	role       string
-	memberlist []string
-	cli        *clientv3.Client
+	id                 int64
+	name               string
+	host               string
+	port               int
+	heartbeatTimer     int64
+	heartbeatKeyPrefix string
+	missionKeyPrefix   string
+	role               string
+	memberlist         []string
+	cli                *clientv3.Client
 }
 
 func main() {
@@ -40,32 +41,34 @@ func main() {
 	}
 
 	mc := monitorCore{
-		id:        time.Now().UnixNano(),
-		name:      viper.GetString("name"),
-		host:      viper.GetString("etcd_host"),
-		port:      viper.GetInt("etcd_port"),
-		heartbeat: viper.GetInt64("heart_beat_timer"),
-		keyprefix: "/heartbeat/monitorcore",
-		role:      "member",
-		cli:       cli,
+		id:                 time.Now().UnixNano(),
+		name:               viper.GetString("name"),
+		host:               viper.GetString("etcd_host"),
+		port:               viper.GetInt("etcd_port"),
+		heartbeatTimer:     viper.GetInt64("heartbeat_timer"),
+		heartbeatKeyPrefix: viper.GetString("heartbeat_key_prefix"),
+		missionKeyPrefix:   viper.GetString("mission_key_prefix"),
+		role:               "member",
+		cli:                cli,
 	}
 
-	mc.Stdout(fmt.Sprintf("ID: %d, Starting monitor...\n", mc.id))
+	mc.Stdout("Starting monitor...")
+	mc.Stdout(fmt.Sprintf("ID: %d", mc.id))
 	defer mc.cli.Close()
 	go func() {
 		mc.SendHeartBeat()
-		for range time.Tick(time.Second * time.Duration(mc.heartbeat)) {
+		for range time.Tick(time.Second * time.Duration(mc.heartbeatTimer)) {
 			mc.SendHeartBeat()
 			mc.UpdateMemberList()
-			mc.Stdout(fmt.Sprintf("Who am I: %s\n", mc.role))
+			mc.Stdout(fmt.Sprintf("Who am I? [%s]%d", mc.role, mc.id))
 		}
 	}()
-	mc.Watch("/mission")
+	mc.Watch()
 }
 
-func (mc *monitorCore) Watch(key string) {
-	mc.Stdout(fmt.Sprintf("Watching by '%s'\n", key))
-	rch := mc.cli.Watch(context.Background(), key, clientv3.WithPrefix())
+func (mc *monitorCore) Watch() {
+	mc.Stdout(fmt.Sprintf("Watching by '%s'", mc.missionKeyPrefix))
+	rch := mc.cli.Watch(context.Background(), mc.missionKeyPrefix, clientv3.WithPrefix())
 	for wresp := range rch {
 		for _, ev := range wresp.Events {
 			fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
@@ -74,9 +77,9 @@ func (mc *monitorCore) Watch(key string) {
 }
 
 func (mc *monitorCore) SendHeartBeat() {
-	mc.Stdout("Send heartbeat")
-	key := fmt.Sprintf("%s/%d", mc.keyprefix, mc.id)
-	resp, err := mc.cli.Grant(context.TODO(), mc.heartbeat+1)
+	mc.Stdout("Send heartbeat to etcd")
+	key := fmt.Sprintf("%s/%d", mc.heartbeatKeyPrefix, mc.id)
+	resp, err := mc.cli.Grant(context.TODO(), mc.heartbeatTimer+1)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -89,17 +92,17 @@ func (mc *monitorCore) SendHeartBeat() {
 
 func (mc *monitorCore) UpdateMemberList() {
 	mc.memberlist = []string{}
-	resp, _ := mc.cli.Get(context.TODO(), mc.keyprefix, clientv3.WithPrefix())
+	resp, _ := mc.cli.Get(context.TODO(), mc.heartbeatKeyPrefix, clientv3.WithPrefix())
 	for _, ev := range resp.Kvs {
 		mc.memberlist = append(mc.memberlist, string(ev.Value[:]))
 	}
 	sort.Strings(mc.memberlist)
-	if(strconv.FormatInt(mc.id, 10) == mc.memberlist[0]){
+	if strconv.FormatInt(mc.id, 10) == mc.memberlist[0] {
 		mc.role = "master"
 	}
 }
 
 func (mc *monitorCore) Stdout(msg string) {
 	t := time.Now()
-	fmt.Printf("[%s] %s\n", t.Format("2017-12-23 23:59:59.000"), msg)
+	fmt.Printf("[%s] %s\n", t.Format("2006-01-02 15:04:05"), msg)
 }
